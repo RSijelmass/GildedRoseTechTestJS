20ba21ed61baa1ab8cd308e08b2d22ea31ec6f28
RSijelmass <R.Sijelmass@gmail.com>
2017-07-13 17:24:30 +0100

    Writes test for Backstage - creates var name for Backstage - writes test for other item

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -5,12 +5,12 @@ describe("Gilded Rose", function() {
 	};
 
 	describe("general rules", function() {
-		it("doesn\'t change the name", function() {
+		it("doesn't change the name", function() {
 			const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
 			const items = gildedRose.updateQuality();
 			expect(items[0].name).toEqual("foo");
 		});
-		it("never let\'s quality go under 0", function() {
+		it("never let's quality go under 0", function() {
 			item = create_item_and_update("bar", 3, 0)
 			expect(item[0].quality).toEqual(0)
 		});
@@ -43,21 +43,41 @@ describe("Gilded Rose", function() {
 	});
 
 	describe("Backstage", function() {
+		var name;
+		beforeEach(function() {
+			name = "Backstage passes to a TAFKAL80ETC concert"
+		});
+		
 		it("increases quality by 1 if sellIn > 10", function() {
-			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 14, 20)
+			item = create_item_and_update(name, 14, 20)
 			expect(item[0].quality).toEqual(21)
 		});
 		it("increases quality by 2 if sellIn ≤ 10", function() {
-			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 8, 20)
+			item = create_item_and_update(name, 8, 20)
 			expect(item[0].quality).toEqual(22)
 		});
 		it("increases quality by 3 if sellIn ≤ 5", function() {
-			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 4, 20)
+			item = create_item_and_update(name, 4, 20)
 			expect(item[0].quality).toEqual(23)
 		});
-	it("never let\'s quality go over 50", function() {
-			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 1, 50)
+		it("never let's quality go over 50", function() {
+			item = create_item_and_update(name, 1, 50)
 			expect(item[0].quality).toEqual(50)
+		});
+		it("sets quality to 0 if sellIn < 0", function() {
+			item = create_item_and_update(name, -2, 5)
+			expect(item[0].quality).toEqual(0)
+		});
+	});
+
+	describe("Other items", function() {
+		it("let's quality decrease by 1 if sellIn ≥ 0 ", function() {
+			item = create_item_and_update("baz", 4, 2)
+			expect(item[0].quality).toEqual(1)
+		});
+	it("let's quality decrease by 2 if sellIn < 0 ", function() {
+			item = create_item_and_update("baz", -4, 2)
+			expect(item[0].quality).toEqual(0)
 		});
 });
 });
