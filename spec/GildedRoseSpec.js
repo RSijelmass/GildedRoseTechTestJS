a97aa716bc411dca9f41653fa192703426e35d42
RSijelmass <R.Sijelmass@gmail.com>
2017-07-13 16:57:27 +0100

    Writes tests to check boundaries of quality (between 0 and 50) - max margin checked both for brie and backstage passes

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -4,20 +4,30 @@ describe("Gilded Rose", function() {
 			return gildedRose.updateQuality();
 	};
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
-  });
+	describe("general rules", function() {
+		it("doesn\'t change the name", function() {
+			const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+			const items = gildedRose.updateQuality();
+			expect(items[0].name).toEqual("foo");
+		});
+		it("never let\'s quality go under 0", function() {
+			item = create_item_and_update("bar", 3, 0)
+			expect(item[0].quality).toEqual(0)
+		});
+	});
 
 	describe("Brie", function() {
 	 	it("quality goes up by one if sellIn ≥ 0", function() {
 			item = create_item_and_update("Aged Brie", 2, 0)
 			expect(item[0].quality).toEqual(1)
 		});
-	it("quality goes up by two if sellIn < 0", function() {
+		it("quality goes up by two if sellIn < 0", function() {
 			item = create_item_and_update("Aged Brie", -1, 0)
 			expect(item[0].quality).toEqual(2)
+		});
+		it("never let\'s quality go over 50", function() {
+			item = create_item_and_update("Aged Brie", -1, 50)
+			expect(item[0].quality).toEqual(50)
 		});
 	});
 
@@ -45,5 +55,9 @@ describe("Gilded Rose", function() {
 			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 4, 20)
 			expect(item[0].quality).toEqual(23)
 		});
-	});
+	it("never let\'s quality go over 50", function() {
+			item = create_item_and_update("Backstage passes to a TAFKAL80ETC concert", 1, 50)
+			expect(item[0].quality).toEqual(50)
+		});
+});
 });
