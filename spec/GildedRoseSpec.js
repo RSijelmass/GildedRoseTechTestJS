9a7d6d67e058b492044996687639c70512379308
RSijelmass <R.Sijelmass@gmail.com>
2017-07-13 13:41:49 +0100

    fix TexttestFixture - some bugs found, inconsistent

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -3,7 +3,7 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gilgedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 
 });
