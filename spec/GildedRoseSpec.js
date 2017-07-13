fb5bd5ae2dad40b435c43c18cc4103ab507515d8
RSijelmass <R.Sijelmass@gmail.com>
2017-07-13 13:49:08 +0100

    Writes two passing tests for quality of brie - goes up by 1 if sellIn ≥ 0 - goes up by 2 if sellIn < 0

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -1,9 +1,23 @@
 describe("Gilded Rose", function() {
+	create_item_and_update = function(name, sellIn, quality) {
+			const gildedRose = new Shop([new Item(name, sellIn, quality)])
+			return gildedRose.updateQuality();
+	};
 
   it("should foo", function() {
-    const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gilgedRose.updateQuality();
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
   });
 
+	describe("Brie", function() {
+	 	it("quality goes up by one if sellIn ≥ 0", function() {
+			item = create_item_and_update("Aged Brie", 2, 0)
+			expect(item[0].quality).toEqual(1)
+		});
+	it("quality goes up by two if sellIn < 0", function() {
+			item = create_item_and_update("Aged Brie", -1, 0)
+			expect(item[0].quality).toEqual(2)
+		});
+});
 });
